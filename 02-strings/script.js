aaa293737b43c518256a4b6e2a808c39e531b86b
RobertoFloris <florisroberto99@gmail.com>
2024-11-12 15:58:42 +0100

    [fix] folder 2 es 3

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -22,7 +22,6 @@ console.log(`
   Il valore è ${bool},
   `)
 
-
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
 // in console la stringa con il nome completo
@@ -30,7 +29,7 @@ const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
 const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
 const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
 
-const fullName = null;
+const fullName = firstName + " " + lastName;
 console.log(fullName); // Output: "Jane Smith"
 
 // 4. Imposta questa variabile utilizzando una proprietà
